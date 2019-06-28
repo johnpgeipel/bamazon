4ce8b2d83845ac96a7c3fbdd96ae9a31d5d7a3c2
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-06-28 12:51:54 -0500

    working on the user prompts and mysql queries upon purchasing selected items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,7 +25,7 @@ connection.connect(function(err) {
 
 //starts the app and initiates conversation with customer
 function start() {
-    console.log("\nWELCOME TO THE BAMAZON STORE!\n".blue);
+    console.log("\nWELCOME TO THE BAMAZON STORE!\n".cyan);
     inquirer.prompt(
         {
             name: "browse",
@@ -43,5 +43,96 @@ function start() {
     });
 }
 
+// function calls and displays products to users
 
+function showItems() {
+    connection.query("SELECT  * FROM products",
+    function(err,res) {
+        if (err) throw err;
+        console.log("\nAll Products\n".cyan.underline);
+        // console.log(res);
+        var products = [],
+        for (var i = 0; i < res.length; i++) {
+            products.push([res[i].item_id, res[i].product_name, res[i].department_name,
+                res[i].price, res[i].stock_quantity]);
+        }
+        var headings = ["Item ID", "Product", "Department", "Price ($)", "Quantity in Stock"];
+        console.log(headings, products);
+    });
+}
+
+function promptUser() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "Please enter the ID number of the item that you would like to purchase.",
+            validate: function(value) {
+                if (value <= 0 || isNaN(value)) {
+                    console.log("\nPlease enter a valid item ID number.\n".red);
+                } else {
+                    return true;
+                }
+            }        
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Please enter the quantity of the item that you wuld like to purchase.",
+            validate: function(value) {
+                if (isNaN(value)) {
+                    console.log("\nPlease enter a valid number.\n.red");
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function(answer) {
+        itemID = answer.id;
+        itemQuantity = answer.quantity;
+
+        connection.query("SELECT * FROM products WHERE  item_id=" + itemID, function(err,res) {
+            selected = res[0];
+
+            if(itemQuantity > selected.stock_quantity && selected.stock_quantity > 1) {
+                statement = "\nWe're sorry, we only have " + selected.stock_quantity + " " + selected.product_name + "s available at this time.\n";
+                console.log(statement.red);
+                promptUser();
+
+            } else if (itemQuantity > selected.stock_quantity && selected.stock_quantity === 1) {
+                statement = "\nWe're sorry, we only have 1 " + selected.product_name + " available at this time.\n";
+                console.log(statement.red);
+                promptUser();
+
+            }else if (itemQuantity > selected.stock_quantity && selected.stock_quantity < 1) {
+                statement : "\nWe're sorry, " + selected.product_name + " is currently out of stock.\n";
+                console.log(statement.red);
+                promptUser();
+
+            } else if (+itemquantity === 1) {
+                statement = "\nYou are purchasing 1 " + selected.product_name + ".";
+                buyProduct();
+
+            } else {
+                statement = "\nYou are purchasing " +
+                itemQuantity + " " + selected.product_name + "s.";
+                buyProduct();
+            }
+        });
+    });
+}
+
+function buyProduct() {
+    inquirer.prompt(
+        {
+            name: "buy",
+            type : "confirm",
+            message: statement + " Would like to check out?"
+        }
+    ).then(function(answer) {
+        if(answer.buy) {
+            connection.query("UPDATE products SET stock_quantity - ? WHERE item_id = ?", [itemQuantity, itemID], function(err, res))
+        }
+    })
+}
 
